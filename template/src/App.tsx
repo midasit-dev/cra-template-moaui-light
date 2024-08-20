bebde5abcb433f22d5864b3e74ed517431639a46
kh1012 <kh1012@midasit.com>
2024-08-20 19:20:55 +0900

    index 및 App tsx 수정

--- template/src/App.tsx
+++ template/src/App.tsx
@@ -1,7 +1,13 @@
 import React from "react";
+import DarkModeExample from "./samples/darkmode";
+import ResponsiveLayout from "./samples/reponsive";
 
 function App() {
-  return <>App</>;
+  return (
+    <h1 className="font-bold text-lg text-l-text dark:text-d-text">
+      import 되어있는 컴포넌트를 사용해보세요.
+    </h1>
+  );
 }
 
 export default App;
--- template/src/index.tsx
+++ template/src/index.tsx
@@ -1,11 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./output.css";
 
-const root = 
-	ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+const root = ReactDOM.createRoot(
+  document.getElementById("root") as HTMLElement
+);
 root.render(
-	<React.Fragment>
-		<App />
-	</React.Fragment>
+  <React.Fragment>
+    <App />
+  </React.Fragment>
 );
